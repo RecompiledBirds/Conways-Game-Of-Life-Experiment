78a4facb845b3174ea6bd87292d5db84e9e346eb
NesiAvali <fiveggamer@gmail.com>
2022-09-17 12:07:59 -0400

    Documentation

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName ="New CellType",menuName ="Cells/New Cell type")]
+public class Cell : ScriptableObject
+{
+    public Sprite baseSprite;
+    public static Sprite sprite;
+    public Color CellColor;
+    public int requiredNeighborsForUnderPopDeath;
+    public int requiredNeighborsForOverPopDeath;
+    public int requiredNeighborsForReproduction;
+    public bool carnivoreCell;
+    public string id;
+    public KeyCode hotKey;
+    public bool ages = false;
+    public int maxAgeInTicks = 100;
+    private Dictionary<int,Tile> tiles = new Dictionary<int,Tile>();
+    //Generate tiles, etc
+    public void Setup()
+    {
+        if (sprite == null)
+            sprite = baseSprite;
+        if (!ages)
+        {
+            Tile tile = (Tile)CreateInstance(typeof(Tile));
+            tile.sprite = baseSprite;
+            tile.color = CellColor;
+            tiles.Add(0,tile);
+        }
+        else
+        {
+            for (int x = 255; x > 0; x -= (255 / maxAgeInTicks))
+            {
+                tiles.Add(x, new Tile()
+                {
+                    color = new Color(CellColor.r, CellColor.g, CellColor.b, x)
+                });
+            }
+        }
+    }
+    public Tile GetTile(int age)
+    {
+        if (!ages)
+        {
+
+        }
+        return tiles[0];
+    }
+
+    public bool Eaten(Cell[,] map, Vector2Int position, int gameBoardSize, out Cell eater)
+    {
+        for (int xPos = position.x - 1; xPos <= position.x + 1; xPos++)
+        {
+            for (int yPos = position.y - 1; yPos <= position.y + 1; yPos++)
+            {
+                //Wrap board
+                int x = xPos;
+                int y = yPos;
+                if (x >= gameBoardSize)
+                    x = 0;
+                if (x < 0)
+                    x = gameBoardSize - 1;
+                if (y < 0)
+                    y = gameBoardSize - 1;
+                if (y >= gameBoardSize)
+                    y = 0;
+
+                if (!(y == position.y && x == position.x))
+                {
+                    if (map[x, y] != null)
+                        if (map[x, y].carnivoreCell)
+                        {
+                            eater = map[x, y];
+                            bool eaten = Random.Range(1, 4) == 3 && eater != this;
+
+                            if (eaten)
+                                map[x, y] = eater;
+                            return eaten;
+                        }
+                }
+            }
+        }
+        eater=null;
+        return false;
+    }
+
+    public int GetNeighbors(Cell[,] map, Vector2Int position,int gameBoardSize)
+    {
+        List<Cell> knownCells = new List<Cell>();
+        float amount = 0;
+        //Move in a 3x3 space around the center to find all possible neighbors
+        for (int xPos = position.x - 1; xPos <= position.x + 1; xPos++)
+        {
+            for (int yPos = position.y - 1; yPos <= position.y + 1; yPos++)
+            {
+                //Wrap board
+                int x = xPos;
+                int y = yPos;
+                if (x >= gameBoardSize)
+                    x = 0;
+                if (x < 0)
+                    x = gameBoardSize - 1;
+                if (y < 0)
+                    y = gameBoardSize - 1;
+                if (y >= gameBoardSize)
+                    y = 0;
+
+                    //Make sure it is not the center cell
+                    if (!(y == position.y && x == position.x))
+                    {
+                        if (map[x, y] != null)
+                        {
+                            knownCells.Add(map[x, y]);
+                            if (!carnivoreCell)
+                                amount += map[x, y] == this ? 1 : 0;
+                            else
+                                amount += (knownCells.Any(x=>x==this)) ? map[x,y]==this? 1 : 1 : 0;
+                        }
+                    }
+            }
+        }
+
+        return Mathf.RoundToInt(amount);
+    }
+
+    public bool Dies(Cell[,] map, Vector2Int position, int gameBoardSize)
+    {
+        //Get all neighbors.
+        int neighbors = GetNeighbors(map, position, gameBoardSize);
+        return neighbors > requiredNeighborsForOverPopDeath || GetNeighbors(map,position,gameBoardSize)<requiredNeighborsForUnderPopDeath || (Eaten(map,position,gameBoardSize,out Cell eater));
+    }
+
+    public bool ShouldBePlacedHere(Cell[,] map, Vector2Int position, int gameBoardSize, out bool eats)
+    {
+        //Get all neighbors.
+        int neighbors = GetNeighbors(map, position, gameBoardSize);
+        eats = false;
+        //Do we eat a cell here?
+        if (carnivoreCell)
+        {
+            eats = map[position.x, position.y] != null;
+        }
+        return requiredNeighborsForReproduction == neighbors;
+    }
+}
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,41 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class Game : MonoBehaviour
 {
     //These are defined in the Unity Inspector tab
-    public Tile cell;
     public Tile empty;
     public Tilemap map;
     public int gameBoardSize;
+    public Cell customCell;
+    public Cell[,] gameBoard;
+    public Cell[,] queue;
+    [SerializeField]
+    public List<Cell> cellTypes  = new List<Cell>();
 
-    public int[,] gameBoard;
-    public int[,] queue;
+
+    //Custom cell varibles
+    string customUnderPop="2";
+    string customOverPop = "3";
+    string customReproducePop = "3";
     // Start is called before the first frame update
     void Start()
     {
     //Create and empty board and queue
-        gameBoard = new int[gameBoardSize,gameBoardSize];
-        queue = new int[gameBoardSize,gameBoardSize];
+        gameBoard = new Cell[gameBoardSize,gameBoardSize];
+        queue = new Cell[gameBoardSize,gameBoardSize];
         //Paint the tilemap
         for(int x = 0; x < gameBoardSize; x++)
         {
             for(int y = 0; y < gameBoardSize; y++)
             {
-                gameBoard[x,y] = 0;
+                gameBoard[x,y] = null;
                 map.SetTile(new Vector3Int(x, y), empty);
                 map.RefreshTile(new Vector3Int(x, y));       
             }
         }
+        foreach(Cell cell in cellTypes)
+        {
+            cell.Setup();
+        }
+        customCell = (Cell)ScriptableObject.CreateInstance(typeof(Cell));
+        customCell.id = "CustomCell";
+        customCell.CellColor = new Color(16, 255, 0, 255);
+        customCell.baseSprite = Cell.sprite;
+        customCell.Setup();
+        selectedCell = cellTypes[0];
+    }
+    public void Reset()
+    {
+        queue = new Cell[gameBoardSize, gameBoardSize];
+        gameBoard = new Cell[gameBoardSize, gameBoardSize];
+        UpdateBoard();
+    }
+    
+    void OnGUI()
+    {
+        Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+        Rect rect = new Rect(new Vector2(0, 0), new Vector2(screenSize.x / 4, screenSize.y));
+        if(GUILayout.Button("Reset (R)"))
+        {
+            Reset();
+        }
+        if(GUILayout.Button(playing?"Pause (Space)":"Unpause (Space)"))
+        {
+            playing = !playing;
+        }
+        if (GUILayout.Button("Save"))
+        {
+            Save();
+        }
+        if (GUILayout.Button("Load"))
+        {
+            Load();
+        }
+        GUILayout.TextArea($"Cell Count: {cellCount}");
+        GUILayout.TextArea($"Sim speed: {speed}");
+        foreach(Cell cell in cellTypes)
+        {
+            GUILayout.TextArea($"{cell.id} ({cell.hotKey}){(selectedCell==cell?"(Current)":"")}");
+        }
+        GUILayout.TextArea($"Custom cell (C): {(selectedCell == customCell ? "(Current)" : "")}");
+        GUILayout.TextArea("Underpopulated at:");
+        customUnderPop = GUILayout.TextField(customUnderPop);
+        GUILayout.TextArea("Overpopulated at:");
+        customOverPop = GUILayout.TextField(customOverPop);
+        GUILayout.TextArea("Reproduces at:");
+        customReproducePop = GUILayout.TextField(customReproducePop);
+        if(GUILayout.Button($"Is Carnivore: {customCell.carnivoreCell}"))
+        {
+            customCell.carnivoreCell = !customCell.carnivoreCell;
+        }
+        customCell.requiredNeighborsForReproduction = int.Parse(customReproducePop);
+        customCell.requiredNeighborsForOverPopDeath = int.Parse(customOverPop);
+        customCell.requiredNeighborsForUnderPopDeath = int.Parse(customUnderPop);
     }
 
     //These are simply used to determine if and how fast the simulation is running.
     bool playing = false;
     float speed=100;
     float tick = 0;
+    //Cell tracking
+    int cellCount;
+    public Cell selectedCell;
     // Update is called once per frame
     void Update()
     {
@@ -55,16 +124,14 @@ public class Game : MonoBehaviour
             //Tick increments by the speed varible
             tick += speed;
         }
-
+        
         //adjust simulation speed
         if (Input.GetKey(KeyCode.Equals))
         {
-            Debug.Log(speed);
             speed += 0.5f;
         }
         if (Input.GetKey(KeyCode.Minus))
         {
-            Debug.Log(speed);
             speed -= 0.5f;
         }
         //Clamp the speed so it never stops, and never goes too fast.
@@ -73,10 +140,20 @@ public class Game : MonoBehaviour
         //reset board if needed
         if (Input.GetKey(KeyCode.R))
         {
-            queue = new int[gameBoardSize, gameBoardSize];
-            gameBoard = new int[gameBoardSize, gameBoardSize];
+            Reset();
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.C))
+            selectedCell = customCell;
+
+        foreach (Cell cell in cellTypes)
+        {
+            if (Input.GetKeyDown(cell.hotKey))
+            {
+                selectedCell = cell;
+                break;
+            }
+        }
         //"Paint" cells with left mouse button
         if (Input.GetKey(KeyCode.Mouse1))
         {
@@ -87,8 +164,8 @@ public class Game : MonoBehaviour
             if (castedPost.x > 0 && castedPost.y > 0 && castedPost.x < gameBoardSize && castedPost.y < gameBoardSize)
             {
                 //Set cell values at position
-                gameBoard[castedPost.x, castedPost.y] = 1;
-                map.SetTile(castedPost, cell);
+                gameBoard[castedPost.x, castedPost.y] =selectedCell;
+                map.SetTile(castedPost, selectedCell.GetTile(0));
                 map.RefreshTile(castedPost);
             }
         }
@@ -97,76 +174,118 @@ public class Game : MonoBehaviour
     /// <summary>
     /// Calculate the new tiles.
     /// </summary>
-    public void PrepareQueue()
+    private void PrepareQueue()
     {
         
         for(int x = 0; x < gameBoardSize; x++)
         {
             for(int y = 0; y < gameBoardSize; y++)
             {
-                //Get neighbors
-                int neighborCount = GetNeighbors(x, y);
-                //Rules
-                switch (gameBoard[x, y])
+                Cell cell = gameBoard[x, y];
+                Vector2Int pos = new Vector2Int(x, y);
+                Cell[] cells = new Cell[cellTypes.Count+1];
+                cellTypes.CopyTo(cells);
+                cells[cells.Length-1]=customCell;
+                //Check if we should place a cell.
+                foreach (Cell cType in cells)
                 {
-                    //Death
-                    case (1):
-                        //underpopulated
-                        if (neighborCount < 2)
-                            queue[x, y] = -1;
-                        //overpopulated
-                        if (neighborCount > 3)
-                        {
-                            queue[x, y] = -1;
-                        }
-                        break;
-                    //Reproduction
-                    case 0:
-                        //Has 3 living neighbors, becomes alive
-                        if (neighborCount == 3)
-                            queue[x, y] = 1;
-                        break;
-
-
-                }
-            }
-        }
-    }
-
-    /// <summary>
-    /// Find all neighbors of a tile.
-    /// </summary>
-    /// <param name="x"></param>
-    /// <param name="y"></param>
-    /// <returns></returns>
-    public int GetNeighbors(int x, int y)
-    {
-        int amount=0;
-        //Move in a 3x3 space around the center to find all possible neighbors
-        for(int xPos = x - 1; xPos <= x+1; xPos++)
-        {
-            for(int yPos = y-1; yPos<=y+1; yPos++)
-            {
-               //Make sure it is not out of bounds
-                if (xPos >= 0 && yPos >= 0 && xPos < gameBoardSize && yPos < gameBoardSize)
-                {
-                //Make sure it is not the center cell
-                    if (!(yPos == y && xPos == x))
+                    if (cType.ShouldBePlacedHere(gameBoard, pos, gameBoardSize,out bool eats))
                     {
-                        amount += gameBoard[xPos, yPos];
+                        queue[x, y] = cType;
                     }
-                    
+                    if (eats)
+                        continue;
+                }
+
+                if (cell == null)
+                    continue;
+               //Check if the cell should die.
+                if(cell.Dies(gameBoard,pos, gameBoardSize))
+                {
+                    queue[x, y] = null;
+                }
+                else
+                {
+                    queue[x,y]= cell;
                 }
             }
         }
-
-        return amount;
     }
 
+    public string SavePath
+    {
+        get
+        {
+            return $"{Application.persistentDataPath}/Save.xml";
+        }
+    }
+
+    public void Save()
+    {
+        XmlDocument save = EncodedSave();
+        save.Save(SavePath);
+    }
+
+    public void Load()
+    {
+        Reset();
+        XmlDocument save = new XmlDocument();
+        save.Load(SavePath);
+        DecodeSave(save);
+    //    UpdateBoard();
+    }
+
+    public void DecodeSave(XmlDocument document)
+    {
+        //Reset the board
+        Reset();
+        //Load the cells from each node
+        foreach(XmlNode child in document.DocumentElement.ChildNodes)
+        {
+            string type = child.Name;
+            Cell selected = cellTypes.Find(x => x.id == type);
+            if (selected == null && type == "CustomCell")
+                selected = customCell;
+            string val = child.InnerText;
+            //Remove ()
+           
+            //Get numbers out
+            string[] values =val.Split(',');
+            //Parse
+            int x = int.Parse(values[0]);
+            int y = int.Parse(values[1]);
+
+            //Set val
+            gameBoard[x, y] = selected;
+            map.SetTile(new Vector3Int(x, y, 0), selected.GetTile(0));
+        }
+    }
+
+    public XmlDocument EncodedSave()
+    {
+        //Root element
+        string xml = "<Game>";
+        XmlDocument document = new XmlDocument();
+        for(int x=0; x<gameBoardSize; x++)
+        {
+            for(int y=0; y<gameBoardSize; y++)
+            {
+                //<CellName>Position<CellName>
+                if(gameBoard[x, y] != null)
+                    xml += $"<{gameBoard[x, y].id}>{x},{y}</{gameBoard[x, y].id}>";
+            }
+        }
+        xml += "</Game>";
+        Debug.Log(xml);
+        //Load into doc
+        document.LoadXml(xml);
+        return document;
+    }
 
     //Update the board with new tiles.
-    public void UpdateBoard()
+    private void UpdateBoard()
     {
+        cellCount = 0;
     //Move through the entire board
         for(int x = 0; x < gameBoardSize; x++)
         {
@@ -174,28 +293,25 @@ public class Game : MonoBehaviour
             {
                 //As both of these are numbers, we can add them together.
                 //This saves time setting values in queue, by only setting relevant values.
-                gameBoard[x, y] += queue[x, y];
-                
-                if (gameBoard[x, y] > 1)
-                    gameBoard[x, y] = 1;
-                    
+                gameBoard[x, y] = queue[x, y];
+               
                 //Get the cell
-                int item = gameBoard[x, y];
-                
+                Cell item = gameBoard[x, y];
+
                 //Paint the map with the new cells
-                switch (item)
+                if (item != null)
                 {
-                    case 0:
-                        map.SetTile(new Vector3Int(x, y), empty);
-                        break;
-                    case 1:
-                        map.SetTile(new Vector3Int(x, y), cell);
-                        break;
+                    map.SetTile(new Vector3Int(x,y,0),item.GetTile(0));
+                    cellCount++;
+                }
+                else
+                {
+                    map.SetTile(new Vector3Int(x, y, 0), empty);
                 }
                 
             }
         }
         //Reset the queue
-        queue = new int[gameBoardSize, gameBoardSize];
+        queue = new Cell[gameBoardSize, gameBoardSize];
     }
 }
